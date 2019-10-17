e499a5adea045bf4edd028a6f60d6d9bed2279f6
Diego_Wang <16307110195@fudan.edu.cn>
2019-10-17 23:30:58 +0800

    "Make functional_tests an app, use LiveServerTest"

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,13 @@
+from django.test import LiveServerTestCase
+# we use Live.. class to rm .sqlite3 auto
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
+#import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     
     def setUp(self):
         self.browser=webdriver.Firefox()
@@ -12,9 +15,15 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
     
-    def test_can_start_a_list_and(self):
-        self.browser.get('http://localhost:8000')
+    def check_for_row_in_list_table(self,row_text):
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
         
+    
+    def test_can_start_a_list_and(self):
+        self.browser.get(self.live_server_url)
+        #use url from server
         self.assertIn('To-Do',self.browser.title)
         header_text=self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do',header_text)
@@ -26,19 +35,17 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox=self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         
-        table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly',
-                      [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.fail('Finish it')
         
-if __name__=='__main__':
-    unittest.main(warnings='ignore')
+#if __name__=='__main__':
+#    unittest.main(warnings='ignore')
 
