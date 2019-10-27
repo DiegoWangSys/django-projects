7bd806c00419f439b03a3a1e2ec530c3c85f03e9
Diego_Wang <16307110195@fudan.edu.cn>
2019-10-27 14:42:45 +0800

    we make a new functional test to fit in diff users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,10 +2,11 @@ from django.test import LiveServerTestCase
 # we use Live.. class to rm .sqlite3 auto
 
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 #import unittest
-
+MAX_WAIT=10
 
 class NewVisitorTest(LiveServerTestCase):
     
@@ -34,18 +35,63 @@ class NewVisitorTest(LiveServerTestCase):
                 'Enter a to-do item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox=self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
         
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        self.fail('Finish it')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+#        self.fail('Finish it')
         
+    def test_multiple_users_can_start_lists_at_diff_urls(self):
+        self.browser.get(self.live_server_url)
+        #send get request
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        
+        edith_list_url=self.browser.current_url
+        self.assertRegex(edith_list_url,'/lists/.+')
+        #we use regular expression to assert current url has the form /lists/
+        #and cookie is for identifying diff clients in HTTP request
+        self.browser.quit()
+        self.browser=webdriver.Firefox()
+        
+        self.browser.get(self.live_server_url)
+        page_text=self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers',page_text)
+        self.assertNotIn('make a fly',page_text)
+        #when a new get request received and cookie is a new one we can not find
+        #table text in edith
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        
+        francis_list_url=self.browser.current_url
+        self.assertRegex(francis_list_url,'/lists/.+')
+        self.assertNotEqual(edith_list_url,francis_list_url)
+        
+        page_text=self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers',page_text)
+        self.assertNotIn('Buy milk',page_text)
+        
+        
+    def wait_for_row_in_list_table(self,row_text):
+        start_time=time.time()
+        while True:
+            try:
+                table=self.browser.find_element_by_id('id_list_table')
+                rows=table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text,[row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException)as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 #if __name__=='__main__':
 #    unittest.main(warnings='ignore')
 
