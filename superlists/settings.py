9da3bd32b363094609cc65ed6e439b6eb137d3c8
Diego_Wang <16307110195@fudan.edu.cn>
2019-12-04 10:44:52 +0800

    confusing changes in settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8qpyxibpohgeh0n3dg+kfy!e$m0i#6buc74d^0_cntf8k^a679'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+TEMPLATE_DEBUG=DEBUG
+
+ALLOWED_HOSTS = ['diegowang.com']
 
 
 # Application definition
